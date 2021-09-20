f2e6e0a30b94fcd7494124d868811908a98d21b8
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-20 08:49:19 -0300

    definindo classe e método construtor de 'pessoa'(ex.2)

--- src/br/com/zup/ListaExercicioModelagemClasses/Pessoa.java
+++ src/br/com/zup/ListaExercicioModelagemClasses/Pessoa.java
@@ -1,5 +1,16 @@
 package br.com.zup.ListaExercicioModelagemClasses;
 
 public class Pessoa {
+    String nome;
+    int idade;
+    double peso;
+    double altura;
+
+    public Pessoa(String nome, int idade, double peso, double altura){
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = 0;
+        this.altura = altura;
+    }
 
 }
