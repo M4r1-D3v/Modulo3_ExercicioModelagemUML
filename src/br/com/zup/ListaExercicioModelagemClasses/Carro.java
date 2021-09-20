f08b4beaa93124a8c3e93f04918d24dc7392fa32
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-20 09:05:59 -0300

    definindo classe e método construtor de 'carro' (ex.4)

---
+++ src/br/com/zup/ListaExercicioModelagemClasses/Carro.java
@@ -0,0 +1,15 @@
+package br.com.zup.ListaExercicioModelagemClasses;
+
+public class Carro {
+    int quantidadePortas;
+    String modelo;
+    String marca;
+    double potencia;
+
+    public Carro(int quantidadePortas, String modelo, String marca, double potencia){
+        this.quantidadePortas = quantidadePortas;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.potencia = 2.0;
+    }
+}
