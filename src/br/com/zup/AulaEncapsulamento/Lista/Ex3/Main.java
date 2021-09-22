e3f4ac1b18bd9213fda60380cb4bcc2913755b66
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-22 16:46:57 -0300

    Criação classe Main e classe Pessoa

--- src/br/com/zup/AulaEncapsulamento/Lista/Ex3/Main.java
+++ src/br/com/zup/AulaEncapsulamento/Lista/Ex3/Main.java
@@ -1,4 +1,7 @@
 package br.com.zup.AulaEncapsulamento.Lista.Ex3;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/br/com/zup/AulaEncapsulamento/Lista/Ex3/Pessoa.java
@@ -0,0 +1,10 @@
+package br.com.zup.AulaEncapsulamento.Lista.Ex3;
+
+import javax.xml.crypto.Data;
+
+public class Pessoa {
+    private String nome;
+    private Data dataNascimento;
+    private double altura;
+
+}
