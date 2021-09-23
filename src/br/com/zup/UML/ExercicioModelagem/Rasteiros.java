4894a2b0b442b1efdf7c8f60c0c14f47a5f39792
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-23 15:53:01 -0300

    Modelando classe Rasteiros (método construtor + getter/setter + método aplicarDano)

--- src/br/com/zup/UML/ExercicioModelagem/Rasteiros.java
+++ src/br/com/zup/UML/ExercicioModelagem/Rasteiros.java
@@ -1,4 +1,23 @@
 package br.com.zup.UML.ExercicioModelagem;
 
-public class Rasteiros {
+public class Rasteiros extends Inimigos {
+    private double danoEspinhos;
+
+    public Rasteiros(String nome, double vida,double danoEspinhos){
+        super(nome, vida);
+        this.danoEspinhos = danoEspinhos;
+
+    }
+
+    public double getDanoEspinhos() {
+        return danoEspinhos;
+    }
+
+    public void setDanoEspinhos(double danoEspinhos) {
+        this.danoEspinhos = danoEspinhos;
+    }
+
+    public void aplicarDano(double danoEspinhos){
+        return danoEspinhos;
+    }
 }
