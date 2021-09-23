66e0b61b907c79a3eec61b7deb7272d6d4b7b1c9
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-23 15:07:46 -0300

    Transformando Gerreiros em subclasse de Heroi + criando getter e setter

--- src/br/com/zup/UML/ExercicioModelagem/Guerreiros.java
+++ src/br/com/zup/UML/ExercicioModelagem/Guerreiros.java
@@ -1,10 +1,18 @@
 package br.com.zup.UML.ExercicioModelagem;
 
-public class Guerreiros {
+public class Guerreiros extends Heroi {
     private double poderAtaque;
 
     public Guerreiros(String nome, double vida, double poderAtaque){
         super(nome, vida);
+        this.poderAtaque = poderAtaque;
+    }
+
+    public double getPoderAtaque() {
+        return poderAtaque;
+    }
+
+    public void setPoderAtaque(double poderAtaque) {
         this.poderAtaque = poderAtaque;
     }
 }
