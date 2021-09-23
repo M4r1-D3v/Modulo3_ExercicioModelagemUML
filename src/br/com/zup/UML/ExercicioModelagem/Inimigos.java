d6e65600795efa2bebf40e322112ea1016fb9304
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-23 14:54:37 -0300

    Criação classe Inimigos mais métodos construtor, getters e setters

--- src/br/com/zup/UML/ExercicioModelagem/Inimigos.java
+++ src/br/com/zup/UML/ExercicioModelagem/Inimigos.java
@@ -5,6 +5,24 @@ public class Inimigos {
     public double vida;
 
     public Inimigos(String nome, double vida){
-        this.nome
+        this.nome = nome;
+        this.vida = vida;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+
 }
